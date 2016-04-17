ac24b22e418a69ade2626a17d038ad2992038192
翁霄驰(xiaochi weng) <wxc267@gmail.com>
2016-04-17 17:37:41 -0400

    more menu

--- Assets/MenuSelect.cs
+++ Assets/MenuSelect.cs
@@ -4,10 +4,17 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MenuSelect : MonoBehaviour {
-
+    GameObject mainMenu;
+    GameObject about;
+    GameObject credit;
 	// Use this for initialization
 	void Start () {
-	
+        mainMenu = GameObject.Find("mainmenu");
+        about = GameObject.Find("about");
+        credit = GameObject.Find("credit");
+        mainMenu.SetActive(true);
+        about.SetActive(false);
+        credit.SetActive(false);
 	}
 	public void Turn_Red()
     {
@@ -23,6 +30,22 @@ public class MenuSelect : MonoBehaviour {
         SceneManager.LoadScene("test");
     }
 	// Update is called once per frame
+    public void Enter_Main_Menu()
+    {
+        mainMenu.SetActive(true);
+        about.SetActive(false);
+        credit.SetActive(false);
+    }
+    public void Enter_Credit()
+    {
+        credit.SetActive(true);
+        mainMenu.SetActive(false);
+        about.SetActive(false);
+    }
+    public void Enter_About()
+    {
+
+    }
 	void Update () {
 	
 	}
