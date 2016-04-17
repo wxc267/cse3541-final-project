c63a114e9f8b7a84cc645dcbb72660ecb12b7bbd
翁霄驰(xiaochi weng) <wxc267@gmail.com>
2016-04-17 13:41:44 -0400

    add menu for game

---
+++ Assets/MenuSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void Turn_Red()
+    {
+        GetComponent<Text>().color = Color.red;
+
+    }
+    public void Turn_White()
+    {
+        GetComponent<Text>().color = Color.white;
+    }
+    public void Enter_Game()
+    {
+        SceneManager.LoadScene("test");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
